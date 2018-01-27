0b3aa1c5dd6f956bd7694b64a5d2ced6af94697d
Aprod <>
2018-01-27 15:57:24 +0100

    win

--- player.php
+++ player.php
@@ -73,7 +73,8 @@ class Player
 //                return $this->getPlayer($gameState)['stack'] * (100-$random);
 //            }
 
-            if ($gameState['current_buy_in'] > $this->bigBlind($gameState)){
+
+            if ($gameState['current_buy_in'] >= $this->bigBlind($gameState)){
                 return 0;
             } else {
                 return $this->minimumBet($gameState) + $gameState['minimum_raise'];
@@ -85,6 +86,10 @@ class Player
 //                $this->log('PLAYERS: ' . $playersCount . ' RAND: ' . $random);
 //                return $this->getPlayer($gameState)['stack'] * (100-$random);
 //            }
+
+            if ($this->hasHighCard($gameState, 11) || ($this->hasPair($gameState, 11) && !$this->hasCommunityCards($gameState)) || ($this->hasCommunityCards($gameState) && $this->hasPairWithCommunityCards($gameState, 11))){
+                return $this->minimumBet($gameState) + $gameState['minimum_raise'];
+            }
 
             if ($gameState['current_buy_in'] > $this->bigBlind($gameState)){
                 return 0;
